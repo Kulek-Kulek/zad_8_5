4d869150c964b3b36db156e3ea5766de0607fbef
Krzysztof Ługowski <k.lugowski@yahoo.com>
2017-11-23 20:48:13 +0100

    Add new files

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+var a = prompt('Enter base value');
+var h = prompt('Enter height value');
+var triangleArea = a*h/2;
+
+ 
+alert('Your triangle area is ' + triangleArea);
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+
+
